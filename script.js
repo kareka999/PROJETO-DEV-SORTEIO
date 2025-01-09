0576385b5b5bb53fab900f4f54f0ad11d616abc8
kareka999 <evertondeoliveira121@gmail.com>
2025-01-09 16:43:11 -0300

    atualização

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const botaoSortear = document.querySelector(".button")
+const botao = document.querySelector(".button")
 
+function botaoClicado (){
+  const inputEntre = Math.ceil (document.querySelector(".input-min").value  )
+  const inputE = Math.floor (document.querySelector(".input-max").value)
+  
+ if(inputEntre>inputE){
+    alert ("O valor inicial deve ser menor que o valor final")
+    return
+ }
 
-function gerandoNumeros(){
-    const inputMin = Math.ceil (document.querySelector(".input-min").value)
-    const inputMax = Math.floor(document.querySelector(".input-max").value)
-
-    if(inputMax > inputMin){
-        const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
-        alert (result)
-    }
-    else {
-        alert("O valor minimo tem que ser MENOR que o valor maximo")
-    }
-    
-
+ const numeroSorteado = Math.floor(Math.random() * (inputEntre - inputE + 1)) + inputE;
+ alert (numeroSorteado)
 }
 
-botaoSortear.addEventListener("click", gerandoNumeros)
 
+
+
+
+
+botao.addEventListener("click", botaoClicado)
