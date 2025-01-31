aa1cbafdf383153bbd49350340af98b9aea90534
kareka999 <evertondeoliveira121@gmail.com>
2025-01-31 15:27:45 -0300

    alteracao nas cores add som

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const botao = document.querySelector(".button")
-const resultadoAnimado = document.getElementById("resultadoAnimado");
+const audio = new Audio ("./assets/ding.mp3")  // Caminho do arquivo de som
 
-function botaoClicado (){
-  const inputEntre = Math.ceil (document.querySelector(".input-min").value  )
-  const inputE = Math.floor (document.querySelector(".input-max").value)
-  
- if(inputEntre>=inputE){
-    alert ("O valor inicial deve ser Menor que o valor maximo")
-    return
- }
 
- const numeroSorteado = Math.floor(Math.random() * (inputE - inputEntre + 1)) + inputEntre;
+function generateNumber(){
 
- resultadoAnimado.textContent = numeroSorteado;
+const min = Math.ceil (document.querySelector(".input-min").value)
+const max = Math.floor (document.querySelector(".input-max").value)
+const resultadoAnimado = document.querySelector("#resultadoAnimado");
+
+// Verifica se os valores são válidos
+if(isNaN(min) || isNaN(max) || min>=max){
+   alert("Por favor, insira valores válidos (o mínimo deve ser menor que o máximo).");
+   return;
+}
+
+ const result =  Math.floor(Math.random() * (max - min + 1)) + min;
+
+ // Toca o som
+ audio.play();
+
+
+
+ // Atualiza o conteúdo da div e exibe com animação
+ resultadoAnimado.textContent = result;
  resultadoAnimado.classList.add("mostrar");
- 
- 
+
+ // Remove a classe após 3 segundos para esconder o número depois
  setTimeout(() => {
-     resultadoAnimado.classList.remove("mostrar");
-   }, 60000);
- 
+   resultadoAnimado.classList.remove("mostrar");
+}, 60000);
+
 }
 
 
@@ -28,4 +38,4 @@ function botaoClicado (){
 
 
 
-botao.addEventListener("click", botaoClicado)
+botao.addEventListener("click",generateNumber)
